680ef958ffa63dfb05041892cc8079a0a7f42223
everaldo <esodevs@gmail.com>
2023-01-26 10:33:55 -0300

    finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-let menu = document.querySelector('#menu-icon');
+/*let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
+}*/
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
 }
